0e8da511fef281b94c28a8850995a991194164c3
helloshalini <shalinijaiswal1101@gmail.com>
2025-05-24 14:45:31 +0530

    update

---
+++ update.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+main().then((res) => {
+    console.log("Connection Successful");
+})
+.catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/test');
+}
+
+const userSchema = new mongoose.Schema({
+  name: String,
+  address: String,
+  age: Number,
+});
+
+const user = mongoose.model("user", userSchema);
+
+user.updateOneAndUpdate( {name: Cheery}, {age: 58})
+.then ( (res) => {
+    console.log(res);
+})
+.catch( (err) => {
+    console.log(err);
+});
